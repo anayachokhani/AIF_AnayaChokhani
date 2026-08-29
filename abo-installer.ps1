3d9e907190a112e9c0dd1e6cf32533032338e34b
tempaccforranduse <tempaccforranduse@gmail.com>
2026-08-29 18:13:12 +0530

    Add ABO downloader

---
+++ abo-installer.ps1
@@ -0,0 +1,3 @@
+mkdir -p data/raw/abo data/external/abo
+wget -P /data/raw/abo https://amazon-berkeley-objects.s3.amazonaws.com/archives/abo-listings.tar
+wget -P /data/raw/abo https://amazon-berkeley-objects.s3.amazonaws.com/archives/abo-images-small.tar
